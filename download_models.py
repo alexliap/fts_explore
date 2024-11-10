388e17b27f6882643fa6088031df55a4ee187cfe
root <alexandrosliapates@gmail.com>
2024-11-10 13:13:51 +0000

    fix fuking issue with uni2ts version that messes up the models

--- download_models.py
+++ download_models.py
@@ -1,5 +1,5 @@
 import gdown
 
 # insert the shareable URL of a Drive folder to download
-url = ""
+url = "https://drive.google.com/drive/folders/1Qi8Oh4qyirVh2zacrvyADJVXvy8z3PCr?usp=drive_link"
 gdown.download_folder(url)
