5bbd96650abb156149bb76e45804f02904099422
root <alexandrosliapates@gmail.com>
2024-12-07 13:15:52 +0000

    update make_env with pyenv

--- cli/env_setup/make_env.sh
+++ cli/env_setup/make_env.sh
@@ -2,6 +2,27 @@
 
 # in order to be able to run the script first run "chmod +x make_env.sh"
 
+echo "Downloading pyenv's dependencies ..."
+
+sudo apt update -y
+sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl git \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+echo "Downloading pyenv ..."
+
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+echo "Downloading python 3.11 ..."
+
+pyenv install 3.11
+
+pyenv local 3.11
+
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 echo "CUSTOM_DATA_PATH=finetune_data" >> .env
